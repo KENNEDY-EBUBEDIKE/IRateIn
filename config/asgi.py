575bb2bb3f0580368270e7961027bf4072670644
kennedy <kennedyeziechina@gmail.com>
2024-02-10 08:34:59 +0100

    asgi bug fix

--- config/asgi.py
+++ config/asgi.py
@@ -12,11 +12,13 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from apps.chat.routing import websocket_urlpatterns
-from apps.chat.consumers import TokenAuthMiddleware
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
 django_asgi_app = get_asgi_application()
+
+from apps.chat.routing import websocket_urlpatterns
+from apps.chat.consumers import TokenAuthMiddleware
 
 application = ProtocolTypeRouter(
     {
